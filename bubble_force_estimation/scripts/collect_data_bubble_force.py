df3e5f4824b4c226563a4642cecb4e2310017e13
mr-mikmik <aglaner@hotmail.com>
2021-11-10 12:30:57 -0500

    implementing data collection with the force-torque sensor

---
+++ bubble_force_estimation/scripts/collect_data_bubble_force.py
@@ -0,0 +1,36 @@
+#! /usr/bin/env python
+import argparse
+
+from bubble_force_estimation.bubble_force_data_collection.bubble_force_data_collection import BubbleForceDataCollection
+
+# TEST THE CODE: ------------------------------------------------------------------------------------------------------
+
+
+def collect_data_force_test(save_path, scene_name, num_data=10, grasp_area_size=(.25, .25), move_length_limits=(0.01, 0.05), grasp_width_limits=(20,30)):
+
+    dc = BubbleForceDataCollection(data_path=save_path, scene_name=scene_name, grasp_area_size=grasp_area_size, move_length_limits=move_length_limits, grasp_width_limits=grasp_width_limits)
+    dc.collect_data(num_data=num_data)
+
+
+if __name__ == '__main__':
+
+    parser = argparse.ArgumentParser('Collect Data Drawing')
+    parser.add_argument('save_path', type=str, help='path to save the data')
+    parser.add_argument('num_data', type=int, help='Number of data samples to be collected')
+    parser.add_argument('--scene_name', type=str, default='drawing_data', help='scene name for the data. For organization purposes')
+    parser.add_argument('--prob_axis', type=float, default=0.08, help='probability for biasing the drawing along the axis')
+    parser.add_argument('--grasp_area_size', type=float, default=(.25, .25), nargs=2, help='size of the y and z semiaxis of the inital grasp area')
+    parser.add_argument('--move_length_limits', type=float, default=(.01, .05), nargs=2, help='(minimum_length, maximum_length) of the move')
+    parser.add_argument('--grasp_width_limits', type=float, default=(20, 30), nargs=2, help='(minimum_width, maximum_width) of the grasp')
+
+    args = parser.parse_args()
+
+    save_path = args.save_path
+    scene_name = args.scene_name
+    num_data = args.num_data
+    grasp_area_size = args.grasp_area_size
+    move_length_limits = args.move_length_limits
+    grasp_width_limits = args.grasp_width_limits
+
+
+    collect_data_force_test(save_path, scene_name, num_data=num_data, grasp_area_size=grasp_area_size, move_length_limits=move_length_limits, grasp_width_limits=grasp_width_limits)
--- bubble_force_estimation/src/bubble_force_estimation/bubble_force_data_collection/bubble_force_data_collection.py
+++ bubble_force_estimation/src/bubble_force_estimation/bubble_force_data_collection/bubble_force_data_collection.py
@@ -5,51 +5,69 @@ import rospy
 from collections import OrderedDict
 import gym
 import copy
+import tf.transformations as tr
 
 from bubble_utils.bubble_data_collection.bubble_data_collection_base import BubbleDataCollectionBase
 from bubble_control.bubble_drawer.bubble_drawer import BubbleDrawer
+from bubble_control.aux.action_sapces import ConstantSpace
+from netft_utils.ft_sensor import FTSensor
+from bubble_utils.bubble_data_collection.wrench_recorder import WrenchRecorder
 
 
-class BubbleDrawingDataCollectionBase(BubbleDataCollectionBase):
+class BubbleForceDataCollection(BubbleDataCollectionBase):
 
-    def __init__(self, *args, impedance_mode=False, **kwargs):
-        self.impedance_mode = impedance_mode
+    def __init__(self, *args, sensor_name='netft', grasp_area_size=(.25, .25), move_length_limits=(0.01, 0.05), grasp_width_limits=(20,30), **kwargs):
+        self.sensor_name = sensor_name
+        self.grasp_area_size = np.asarray(grasp_area_size)
+        self.move_length_limits = move_length_limits
+        self.grasp_width_limits = grasp_width_limits
         self.action_space = self._get_action_space()
         super().__init__(*args, **kwargs)
-        self.last_undeformed_fc = None
+        self.ft_sensor = FTSensor(ns=self.sensor_name)
+        self.ft_sensor_wrench_recorder = WrenchRecorder(wrench_topic=self.ft_sensor.topic_name, scene_name=self.scene_name, save_path=self.save_path, wrench_name='external_wrenches')
 
-    @abc.abstractmethod
     def _get_action_space(self):
-        pass
+        action_space_dict = OrderedDict()
+        action_space_dict['start_point'] = gym.spaces.Box(-self.grasp_area_size, self.grasp_area_size, (2,), dtype=np.float64)  # random uniform (y, z) axes
+        action_space_dict['start_theta'] = ConstantSpace(0.)
+        action_space_dict['direction'] = gym.spaces.Box(low=0, high=2*np.pi, shape=())
+        action_space_dict['length'] = gym.spaces.Box(low=self.move_length_limits[0], high=self.move_length_limits[1], shape=())
+        action_space_dict['grasp_width'] = gym.spaces.Box(low=self.grasp_width_limits[0], high=self.grasp_width_limits[1], shape=())
+        action_space = gym.spaces.Dict(action_space_dict)
+        return action_space
 
-    def _record_gripper_calibration(self):
-        self.med.set_grasp_pose()
-        _ = input('Press enter to open the gripper and calibrate the bubbles')
-        self.med.open_gripper()
-        self.last_undeformed_fc = self.get_new_filecode(update_pickle=False)
-        self._record(fc=self.last_undeformed_fc)
-        _ = input('Press enter to close the gripper')
-        self.med.set_grasping_force(5.0)
-        self.med.gripper.move(25.0)
-        self.med.grasp(20.0, 30.0)
-        rospy.sleep(2.0)
-        print('Calibration is done')
-        self.med.home_robot()
+    def _check_valid_action(self, action):
+        # TODO: Check if
+        valid_action = True
+        return valid_action
 
-    def collect_data(self, num_data):
-        print('Calibration undeformed state, please follow the instructions')
-        self._record_gripper_calibration()
-        out = super().collect_data(num_data)
-        self.med.home_robot()
-        return out
+    def _get_wrench_recording_frames(self):
+        wrench_recording_frames = super()._get_wrench_recording_frames() + [
+                                   '{}_sensor'.format(self.sensor_name),
+                                   '{}_tool_frame'.format(self.sensor_name),
+                                   '{}_tool_tip_frame'.format(self.sensor_name)]
+        return wrench_recording_frames
+
+    def _get_recording_frames(self):
+        child_frames = super()._get_recording_frames() + [
+                        '{}_sensor'.format(self.sensor_name),
+                        '{}_tool_frame'.format(self.sensor_name),
+                        '{}_tool_tip_frame'.format(self.sensor_name)]
+        return child_frames
+
+    def _record(self, fc=None):
+        # add the wrench of the FTSensor to the recorded topics
+        self.ft_sensor_wrench_recorder.record(fc=fc, frame_names=self._get_wrench_recording_frames())
+        super()._record(fc=fc)
 
     def _get_legend_column_names(self):
         action_keys = self._sample_action().keys()
-        column_names = ['Scene', 'UndeformedFC', 'InitialStateFC',  'FinalStateFC', 'GraspForce'] + list(action_keys)
+        column_names = ['Scene', 'Time', 'UndeformedFC', 'InitialStateFC',  'FinalStateFC', 'GraspForce'] + list(action_keys)
         return column_names
 
     def _get_legend_lines(self, data_params):
         legend_lines = []
+        undef_fc_i = data_params['undef_fc']
         init_fc_i = data_params['initial_fc']
         final_fc_i = data_params['final_fc']
         grasp_force_i = data_params['grasp_force']
@@ -57,7 +75,7 @@ class BubbleDrawingDataCollectionBase(BubbleDataCollectionBase):
         scene_i = self.scene_name
         action_keys = self._sample_action().keys()
         action_values = [action_i[k] for k in action_keys]
-        line_i = [scene_i, self.last_undeformed_fc, init_fc_i, final_fc_i,  grasp_force_i] + action_values
+        line_i = [scene_i, undef_fc_i, init_fc_i, final_fc_i,  grasp_force_i] + action_values
         legend_lines.append(line_i)
         return legend_lines
 
@@ -65,17 +83,35 @@ class BubbleDrawingDataCollectionBase(BubbleDataCollectionBase):
         action_i = self.action_space.sample()
         return action_i
 
-    def _do_pre_action(self, action):
-        pass
+    def _get_sensor_tool_pose(self, ref_frame='med_base'):
+        tool_frame_name = '{}_tool_frame'.format(self.sensor_name)
+        rf_T_tf = self.tf2_listener.get_transform(ref_frame, tool_frame_name)
+        tool_pose_rf = self.med._matrix_to_pose(rf_T_tf)
+        return tool_pose_rf
 
-    def _do_action(self, action):
-        pass
+    def _get_sensor_tool_tip_pose(self, ref_frame='med_base'):
+        tool_frame_name = '{}_tool_tip_frame'.format(self.sensor_name)
+        rf_T_tf = self.tf2_listener.get_transform(ref_frame, tool_frame_name)
+        tool_pose_rf = self.med._matrix_to_pose(rf_T_tf)
+        return tool_pose_rf
 
-    def _do_post_action(self, action):
-        pass
+    def _set_robot_position_sensor_tool_frame(self, y, z, theta, ref_frame=None):
+        if ref_frame is None:
+            ref_frame = '{}_tool_frame'.format(self.sensor_name)
+        target_position = np.array([0, y, z]) # in ref_frame
+        # compute orientation -- theta is the rotation along the x axis of the sensor
+        # base_quat = np.array([0, 0, 0, 1.0])
+        base_quat = tr.quaternion_from_euler(np.pi, 0, np.pi, axes='sxyz') # rpy
+        rotation_quat = tr.quaternion_about_axis(theta, axis=np.array([1, 0, 0]))
+        target_quat = tr.quaternion_multiply(rotation_quat, base_quat)                          # in ref_frame # TODO: Account for the grasp frame orientation
+        target_pose = np.concatenate([target_position, target_quat])
+        self.med.plan_to_pose(self.med.arm_group, ee_link_name='grasp_frame', target_pose=list(target_pose), frame_id=ref_frame)
 
-    def _check_valid_action(self, action):
-        return True
+    def _cartesian_delta_motion_sensor_tool_frame(self, delta_y, delta_z, ref_frame=None):
+        if ref_frame is None:
+            ref_frame = '{}_tool_frame'.format(self.sensor_name)
+        delta_pos = np.array([0, delta_y, delta_z])
+        self.med.cartesian_delta_motion(delta_pos, ref_frame=ref_frame)
 
     def _collect_data_sample(self, params=None):
         """
@@ -93,25 +129,39 @@ class BubbleDrawingDataCollectionBase(BubbleDataCollectionBase):
             is_valid_action = self._check_valid_action(action_i)
 
         # Sample the fcs:
+        undef_fc = self.get_new_filecode()
         init_fc = self.get_new_filecode()
         final_fc = self.get_new_filecode()
 
         grasp_force_i = 0  # TODO: read grasp force
 
-        # Set the grasp width
-        self._do_pre_action(action_i)
+        # Move to initial configuration
+        self.med.gripper.move(100, 80)
+        start_point = action_i['start_point']
+        self._set_robot_position_sensor_tool_frame(action_i['start_point'][0], action_i['start_point'][0], action_i['start_theta'])
+
+        # calibrate
+        self.ft_sensor.zero()
+        rospy.sleep(1.0)
+        self._record(fc=undef_fc)
+        # grasp
+        grasp_width = action_i['grasp_width'] # TODO Consider using a force instead of a width
+        self.med.gripper.move(grasp_width, 20)
 
         # record init state:
         self._record(fc=init_fc)
 
-        # draw
-        self._do_action(action_i)
+        # move
+        final_point = start_point + action_i['length'] * np.array([np.cos(action_i['direction']), np.sin(action_i['direction'])])
+        self._cartesian_delta_motion_sensor_tool_frame(final_point[0], final_point[1])
 
         # record final_state
         self._record(fc=final_fc)
 
-        self._do_post_action(action_i)
+        # reset motion
+        self.med.gripper.open_gripper()
 
+        data_params['undeformed_fc'] = undef_fc
         data_params['initial_fc'] = init_fc
         data_params['final_fc'] = final_fc
         data_params['grasp_force'] = grasp_force_i
@@ -119,119 +169,3 @@ class BubbleDrawingDataCollectionBase(BubbleDataCollectionBase):
 
         return data_params
 
-
-class AxisBiasedDirectionSpace(gym.spaces.Space):
-    """
-    Saple space between [0,2pi) with bias towards the axis directions.
-    On prob_axis, the sample will be along one of the cartesian axis directions, i.e. [0, pi/2, pi, 3pi/2]
-    """
-    def __init__(self, prob_axis, seed=None):
-        self.prob_axis = prob_axis
-        super().__init__((), np.float32, seed)
-
-    def sample(self):
-        p_axis_direction = self.np_random.random() # probability of getting an axis motion
-        if p_axis_direction < self.prob_axis:
-            direction_i = 0.5 * np.pi * np.random.randint(0, 4) # axis direction (0, pi/2, pi/ 3pi/2)
-        else:
-            direction_i = np.random.uniform(0, 2 * np.pi)  # direction as [0, 2pi)
-        return direction_i
-
-    def contains(self, x):
-        return 0 <= x <= 2*np.pi
-
-
-class ConstantSpace(gym.spaces.Space):
-    """
-    Constant space. Only has one possible value. For convenience.
-    """
-    def __init__(self, value, seed=None):
-        self.value = value
-        super().__init__((), np.float32, seed)
-
-    def sample(self):
-        return self.value
-
-    def contains(self, x):
-        return x == self.value
-
-    def __eq__(self, other):
-        return (
-                isinstance(other, ConstantSpace)
-                and self.value == other.value
-        )
-
-
-class BubbleDrawingDataCollection(BubbleDrawingDataCollectionBase):
-
-    def __init__(self, *args, prob_axis=0.08, drawing_area_center=(0.55, 0.), drawing_area_size=(.15, .15), drawing_length_limits=(0.01, 0.15), **kwargs):
-        self.prob_axis = prob_axis
-        self.previous_end_point = None
-        self.previous_draw_height = None
-        self.drawing_area_center_point = np.asarray(drawing_area_center)
-        self.drawing_area_size = np.asarray(drawing_area_size)
-        self.drawing_length_limits = drawing_length_limits
-        super().__init__(*args, **kwargs)
-
-    def _get_action_space(self):
-        drawing_area_center_point = self.drawing_area_center_point
-        drawing_area_size = self.drawing_area_size
-
-        action_space_dict = OrderedDict()
-        action_space_dict['lift'] = gym.spaces.MultiBinary(1)
-        action_space_dict['start_point'] = gym.spaces.Box(drawing_area_center_point - drawing_area_size,
-                                          drawing_area_center_point + drawing_area_size, (2,), dtype=np.float64) # random uniform
-        action_space_dict['end_point'] = gym.spaces.Box(drawing_area_center_point - drawing_area_size,
-                                          drawing_area_center_point + drawing_area_size, (2,), dtype=np.float64) # placeholder for the limits
-        action_space_dict['direction'] = AxisBiasedDirectionSpace(prob_axis=self.prob_axis)
-        action_space_dict['length'] = gym.spaces.Box(low=self.drawing_length_limits[0], high=self.drawing_length_limits[1], shape=())
-        action_space_dict['grasp_width'] = ConstantSpace(20.)
-
-        action_space = gym.spaces.Dict(action_space_dict)
-        return action_space
-
-    def _check_valid_action(self, action):
-        end_point = action['end_point']
-        valid_action = end_point in self.action_space['end_point'] # since the end point is computed by ourselves based on direction and length, verify that the end point is within its limits,
-        return valid_action
-
-    def _sample_action(self):
-        action_sampled = super()._sample_action()
-        lift = action_sampled['lift']
-        if self.previous_end_point is None:
-            action_sampled['lift'][0] = 1
-        elif lift == 0:
-            # if we do not lift, start at the end point
-            action_sampled['start_point'] = copy.deepcopy(self.previous_end_point)
-        start_point_i = action_sampled['start_point']
-        length_i = action_sampled['length']
-        direction_i = action_sampled['direction']
-        action_sampled['end_point'] = start_point_i + length_i * np.array([np.cos(direction_i), np.sin(direction_i)])
-
-        return action_sampled
-
-    def _do_pre_action(self, action):
-        start_point_i = action['start_point']
-        grasp_width_i = action['grasp_width']
-        lift = action['lift']
-        self.med.gripper.move(grasp_width_i, 10.0)
-        # Init the drawing
-        if lift == 1:
-            self.med._end_raise()
-            draw_height = self.med._init_drawing(start_point_i)
-            self.previous_draw_height = copy.deepcopy(draw_height)
-        else:
-            draw_height = self.previous_draw_height
-        action['draw_height'] = draw_height # Override action to add draw_height so it is available at _do_action
-
-    def _do_action(self, action):
-        end_point_i = action['end_point']
-        draw_height = action['draw_height']
-        self.med._draw_to_point(end_point_i, draw_height)
-
-
-    def _do_post_action(self, action):
-        end_point_i = action['end_point']
-        self.previous_end_point = copy.deepcopy(action['end_point'])
-        # raise the arm at the end
-        # Raise the arm when we reach the last point
