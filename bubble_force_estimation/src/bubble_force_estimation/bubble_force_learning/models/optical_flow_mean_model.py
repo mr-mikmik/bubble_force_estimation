4736ef010e2de69016ad86f233c78492799c4496
mr-mikmik <aglaner@hotmail.com>
2021-11-28 17:14:22 -0500

    adding trainer and optical flow mean model

---
+++ bubble_force_estimation/src/bubble_force_estimation/bubble_force_learning/models/optical_flow_mean_model.py
@@ -0,0 +1,77 @@
+import torch
+import torch.nn as nn
+import torch.nn.functional as F
+import pytorch_lightning as pl
+import matplotlib.pyplot as plt
+from matplotlib import cm
+import torchvision
+import numpy as np
+import os
+import sys
+
+from bubble_control.bubble_learning.models.aux.fc_module import FCModule
+
+
+class OpticalFlowMeanModel(pl.LightningModule):
+    """Model composed by a fc network that takes the mean of the optical flow and predicts the wrench"""
+    def __init__(self, input_sizes):
+        super().__init__()
+        self.spring_model = self._get_spring_model()
+
+    @classmethod
+    def get_name(cls):
+        return 'optical_flow_mean_model'
+
+    @property
+    def name(self):
+        return self.get_name()
+
+    def _get_spring_model(self):
+        sizes = self._get_sizes()
+        in_size = sizes['optical_flow_mean_r'] + sizes['optical_flow_mean_l']
+        out_size = sizes['def_wrench_ext']
+        model_sizes = [in_size] + [self.fc_h_dim] * self.num_fcs + [out_size]
+        spring_model = FCModule(sizes=model_sizes, skip_layers=self.skip_layers, activation=self.activation)
+        return spring_model
+
+    def forward(self, optical_flow_mean_r, optical_flow_mean_l):
+        optical_flow_mean = torch.cat([optical_flow_mean_r, optical_flow_mean_l], dim=-1)
+        predicted_wrench = self.spring_model(optical_flow_mean)
+        return predicted_wrench
+
+    def _get_sizes(self):
+        sizes = self.input_sizes
+        return sizes
+
+    def _step(self, batch, batch_idx, phase='train'):
+        optical_flow_mean_r = batch['optical_flow_mean_r']
+        optical_flow_mean_l = batch['optical_flow_mean_l']
+        wrench_ext_gth = batch['def_wrench_ext']
+        # Query the model:
+        wrench_ext_pred = self.forward(optical_flow_mean_r, optical_flow_mean_l)
+        # Compute loss:
+        loss = self._compute_loss(wrench_ext_pred, wrench_ext_gth)
+
+        self.log('{}_batch'.format(phase), batch_idx)
+        self.log('{}_loss'.format(phase), loss)
+
+        return loss
+
+    def training_step(self, train_batch, batch_idx):
+        loss = self._step(train_batch, batch_idx, phase='train')
+        return loss
+
+    def validation_step(self, val_batch, batch_idx):
+        loss = self._step(val_batch, batch_idx, phase='val')
+        return loss
+
+    def _compute_loss(self, wrench_ext_pred, wrench_ext_gth):
+        wrench_prediction_loss = self.mse_loss(wrench_ext_pred, wrench_ext_gth)
+        loss = wrench_prediction_loss
+        return loss
+
+    def configure_optimizers(self):
+        optimizer = torch.optim.Adam(self.parameters(), lr=self.lr)
+        return optimizer
+
+
