63b195a680a41a996fd427c12287db160b0c7a58
mr-mikmik <aglaner@hotmail.com>
2022-09-28 11:22:54 -0400

    updating ft_link location and adding ft_urdf_generator

---
+++ bubble_force_estimation/src/bubble_force_estimation/ft_urdf/ft_urdf_generator.py
@@ -0,0 +1,33 @@
+import rospy
+import roslaunch
+import os
+import rospkg
+import subprocess
+from std_srvs.srv import Trigger, TriggerResponse
+
+
+class FTURDFGenerator(object):
+    """
+    Class to automate the call of bubble_force_setup.launch.
+    This launches the urdf collision geometry of the tool
+    """
+    # TODO: Add option to handle multiple tools
+
+    def __init__(self):
+        self.package_name = 'bubble_force_estimation'
+        self.launch_file = 'ft_urdf.launch' # name of the file to be launched inside self.package_name
+        self.node_name = 'ft_urdf_generator'
+        self.proc = None
+
+    def _start_node(self):
+        command = 'roslaunch {0} {1}'.format(self.package_name, self.launch_file)
+        self.proc = subprocess.Popen(command, shell=True)
+
+    def _close_node(self):
+        if self.proc is not None:
+            self.proc.kill()
+
+    def generate(self):
+        self._close_node()
+        self._start_node()
+
